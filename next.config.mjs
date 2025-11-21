e4777c2e535bf89980c9aa42c0a1ee64d087af1f
Adarsh Kumar <adarshkr1375@gmail.com>
2025-11-21 13:02:35 +0000

    Try fixing this error: `Runtime Error: Error: an ENS name used for a con

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      },
-      {
-        protocol: 'https',
-        hostname: 'gateway.pinata.cloud',
-      }
-    ],
-  },
+    // any future config will go here
 };
 
 export default nextConfig;
