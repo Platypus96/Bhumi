c3c434c4b8fc9eb416eda78a34fb96af8cd2eb95
Adarsh Kumar <adarshkr1375@gmail.com>
2025-11-21 21:43:45 +0000

    you have put all the api keys somewhere find it and put to .end

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-5441956411-77100",
-  "appId": "1:118506574253:web:0e0ef41ff26cc854bb5124",
-  "apiKey": "AIzaSyBHlTteUvVRLEghyNTCDZFi67KSKlr_LI8",
-  "authDomain": "studio-5441956411-77100.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "118506574253"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
