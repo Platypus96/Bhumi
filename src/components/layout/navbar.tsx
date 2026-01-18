26848424c15fae44e903402cdff79ab0d8b4ebba
Adarsh Kumar <adarshkr1375@gmail.com>
2026-01-18 08:26:37 +0000

    in the navbar the logo and connect wallet are touching the edges add spa

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import Link from "next/link";
@@ -100,7 +101,7 @@ const BaseNavbar = () => {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center">
+      <div className="container flex h-16 items-center px-4 sm:px-6 lg:px-8">
         <Link href="/" className="mr-6 flex items-center space-x-2">
           <div className="text-primary">
             <University className="h-7 w-7" />
