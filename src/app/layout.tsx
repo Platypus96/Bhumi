daa51f07b05b19e090c79a899697d22718b70033
Adarsh Kumar <adarshkr1375@gmail.com>
2026-01-24 20:10:12 +0000

    change the favicon to website's logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
        <head>
         <link
           rel="icon"
-          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='black' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M4 22h16'/><path d='M2 18v-1l10-4 10 4v1'/><path d='M12 2v8'/><path d='M6 18v-5'/><path d='M18 18v-5'/><path d='M2 13l10 4 10-4'/></svg>"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='black' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><circle cx='12' cy='10' r='3'/><path d='M22 20V8l-10-5-10 5v12h20z'/><path d='M6 17v-5'/><path d='M18 17v-5'/></svg>"
         />
       </head>
       <body 
